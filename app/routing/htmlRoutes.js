a795a1c0742359baa63af7aeef731ca4232f306a
Maddie <madeleinecgribble@gmail.com>
2019-12-17 13:18:10 +0800

    Can add new friends using POST request

---
+++ app/routing/htmlRoutes.js
@@ -0,0 +1,9 @@
+const path = require("path");
+
+module.exports = function(app) {
+  app.get("/survey", function(req, res) {
+    res.sendFile(path.join(__dirname, "app", "public", "survey.html"));
+  });
+
+  //other routes..
+};
--- server.js
+++ server.js
@@ -1,8 +1,11 @@
 const express = require("express");
 const path = require("path");
+const fs = require("fs");
 
 const app = express();
 const PORT = 3000;
+
+let friendsDatabase = JSON.parse(fs.readFileSync("app/data/friends.json", "utf8"));
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
@@ -11,6 +14,22 @@ app.listen(PORT, function() {
   console.log(`App listening on PORT ${PORT}`);
 });
 
-app.get("/", function(req, res) {
-  res.send("Testing");
+app.get("/home", function(req, res) {
+  res.sendFile(path.join(__dirname, "app", "public", "home.html"));
+});
+
+app.get("/survey", function(req, res) {
+  res.sendFile(path.join(__dirname, "app", "public", "survey.html"));
+});
+
+app.get("/api/friends", function(req, res) {
+  res.send(friendsDatabase);
+});
+
+app.post("/api/friends", (req, res) => {
+  friendsDatabase.data.push(req.body);
+  let databaseString = JSON.stringify(friendsDatabase, null, 3);
+  fs.writeFileSync("app/data/friends.json", databaseString);
+  res.send("Friend added");
+  console.log(friendsDatabase);
 });
